5453c957b474b90d9b3cce0bab38f51092195dcb
bogeychan <bogeychan@outlook.com>
2024-07-08 19:54:02 +0200

    🔧fix: exports in package.json

---
+++ test/node/cjs/index.js
@@ -0,0 +1,13 @@
+// if ("Bun" in globalThis) {
+//   throw new Error("❌ Use Node.js to run this test!");
+// }
+
+// const { Stream } = require("@elysiajs/stream");
+
+// if (typeof Stream !== "function" && Stream["name"] === "Stream") {
+//   throw new Error("❌ CommonJS Node.js failed");
+// }
+
+// console.log("✅ CommonJS Node.js works!");
+
+console.log("⏩ CommonJS Node.js unsupported");
